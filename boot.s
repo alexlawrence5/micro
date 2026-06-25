f611aefd22b51eb68ca2cef69e89810e57c8e7b8
Miku Jr. <axfootballcards@gmail.com>
2026-06-25 19:47:15 +0200

    Add bootloader assembly code in boot.s

---
+++ boot.s
@@ -0,0 +1,47 @@
+BITS 16
+ORG 0x7C00
+
+start:
+        cli
+        xor ax, ax
+        mov ds, ax
+        mov es, ax
+        mov ss, ax
+        mov sp, 0x7C00
+        sti
+        mov si, msg
+        call print
+        mov si, smsg
+        call print
+        mov ah, 0x02
+        mov al, 4
+        mov ch, 0
+        mov cl, 2
+        mov dh, 0
+        mov bx, 0x8000
+        int 0x13
+        jc derr
+        jmp 0x0000:0x8000
+
+derr:
+        mov si, err
+        call print
+        hlt
+
+print:
+    lodsb
+    or al, al
+    jz done
+    mov ah, 0x0E
+    int 0x10
+    jmp print
+
+done:
+    ret
+
+msg db "MicroBoot (C) developed by Alex Lawrence.", 0x0D,0x0A,0
+smsg db "!!: Loading userland...",0
+err db "!E!: Disk error", 0
+
+times 510-($-$$) db 0
+dw 0xAA55
